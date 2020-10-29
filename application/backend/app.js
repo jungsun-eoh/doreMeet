252a9b4d09e5256e562bde0df94f34e461771263
jungsun-eoh <jungsun.eoh@gmail.com>
2020-10-29 01:22:35 -0700

    db /not working yet.

---
+++ application/backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+//const port = 3301;
+const mysql = require('mysql');
+
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "newuser",
+//     password: "password"
+//   });
+  
+//   db.connect(function(err) {
+//     if (err) throw err;
+//         console.log("Connected!");
+//   });
+
+
+app.get("/", (req, res) => res.send(
+    "hella @@"
+));
+
+app.get("/insert", (req, res) => {
+    db.query('INSERT INTO name (first_name, art_category) VALUES ("June", "Music")', (err, result) => {
+        if (err) throw err;
+            console.log("Connected!");
+
+        res.send(result);
+    })
+})
+//app.listen(port, () => console.log(`*****Example app listening on port ${port}!`));
+app.listen(3001, () => {
+    console.log("server runing")
+})
---
+++ application/backend/database.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "newuser",
+  password: "password"
+});
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    // db.query("CREATE DATABASE mydb", function (err, result) {
+    //   if (err) throw err;
+    //   console.log("Database created");
+    // });
+  });
