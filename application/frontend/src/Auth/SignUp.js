a5a1e0bf0398b914ba6649acdf2fb6980a16be8d
Nimiksha <nimiksha98@gmail.com>
2020-11-12 02:06:25 -0800

    Updated Sign Up page

--- application/frontend/src/Auth/SignUp.js
+++ application/frontend/src/Auth/SignUp.js
@@ -7,10 +7,14 @@ import './Auth.css';
 
 class SignUp extends Component {
     state = {
-        name: '',
+        firstname: '',
+        lastname: '',
         username: '',
         email: '',
+        gender: '',
         dob: '',
+        phone: '',
+        skill: '',
         password: '',
         confirmpassword: '',
       }
@@ -32,20 +36,28 @@ class SignUp extends Component {
         <>
         <NavbarHome />
         <div className="container">
-            <form className="white" onSubmit={console.log('Submit')}>
+            <form onSubmit={console.log('Submit')}>
             <h1 className="heading">Sign Up</h1>
             <p>Please fill the form to create an account with DoReMeet.</p>
             <small> All fields are required. </small>
 
             <div className="input">
-                <label htmlFor="name" className="sub-heading"> <b> Name </b>
-                <input type="name" id='name' placeholder="Enter Name" required onChange={this.handleChange} /></label>
+                
+                <table>
+                    <tr><td>
+                    <label htmlFor="firstname" className="sub-heading"> <b> First Name </b>
+                <input type="name" id='firstname' placeholder="Enter First Name" required onChange={this.handleChange} /></label>
+                </td><td>
+                <label htmlFor="lastname" className="sub-heading"> <b> Last Name </b>
+                <input type="name" id='lastname' placeholder="Enter Last Name" required onChange={this.handleChange} /></label>
+                </td></tr>
+                </table>
             </div>
 
             <div className="input">
                 <label htmlFor="username" className="sub-heading"> <b> Username </b>
                 <input type="username" id='username' placeholder="Enter Username" required onChange={this.handleChange} /></label>
-            </div>
+                </div>
 
             <div className="input">
                 <label htmlFor="email" className="sub-heading"> <b> Email </b>
@@ -53,8 +65,51 @@ class SignUp extends Component {
             </div>
 
             <div className="input">
-                <label htmlFor="dob" className="sub-heading"> <b> Date of Birth (MM/DD/YYYY) </b>
-                <input type="dob" id="dob" placeholder="Enter Date of Birth" required onChange={this.handleChange}/></label>
+                <label htmlFor="dob" className="sub-heading"> <b> Date of Birth (MM/DD/YYYY)</b>
+                <input type="date" id="dob" placeholder="Enter Date of Birth" required onChange={this.handleChange}/></label>
+            </div>
+
+            <div className="input">
+                <table>
+                <tr><td>
+                 <label htmlFor="gender" className="sub-heading"> <b> Gender </b></label> 
+                 </td>&nbsp;  &nbsp; &nbsp;  &nbsp;
+                <td>
+                <input type="radio" id='gender' onChange={this.handleChange} /> &nbsp; <label>Male</label>
+                </td> &nbsp;  &nbsp; <td>
+                <input type="radio" id='gender' onChange={this.handleChange} />  &nbsp;  <label>Female</label>
+                </td> &nbsp;  &nbsp; <td>
+                <input type="radio" id='gender' onChange={this.handleChange} /> &nbsp; <label>Other</label>
+                </td>
+                 </tr>
+                </table>
+            </div>
+
+            <div className="input">
+                <table><tr><td>
+                <label htmlFor=""className="sub-heading"> <b> Skill Level </b> &nbsp;  &nbsp; &nbsp; </label>
+                </td> <td> 
+                <select type="radio" required onChange={this.handleChange} source="[id=skill]">
+                <option value={"B"}>Begginer</option>
+                <option value={"I"}>Intermediate</option>
+                <option value={"A"}>Advanced</option>
+                <option value={"1"}>1</option>
+                <option value={"2"}>2</option>
+                <option value={"3"}>3</option>
+                <option value={"4"}>4</option>
+                <option value={"5"}>5</option>
+                <option value={"6"}>6</option>
+                <option value={"7"}>7</option>
+                <option value={"8"}>8</option>
+                <option value={"9"}>9</option>
+                <option value={"10"}>10</option>
+                </select>
+                </td></tr></table>
+            </div>
+
+            <div className="input">
+                <label htmlFor="phone" className="sub-heading"> <b> Phone Number </b>
+                <input type="phone" id='phone' placeholder="Enter Phone Number" required onChange={this.handleChange} /></label>
             </div>
 
             <div className="input">
@@ -65,22 +120,20 @@ class SignUp extends Component {
             <div className="input">
                 <label htmlFor="confirmpassword" className="sub-heading"> <b> Confirm Password </b></label>
                 <input type="password" id="confirmpassword" placeholder="Confirm Password" required onChange={this.handleChange} />
-            </div>
+            </div> 
             
             <div className="input">
                 <Button className='btn' buttonStyle='btn--primary' buttonSize='btn--large'> Sign Up </Button>
             </div>
-            
+
             &nbsp; 
             
             <div className="input">
                 <label><input type="checkbox" checked="checked" name="remember" /> Remember Me </label>
             </div>
-
+            
             <p>By creating an account you agree to our Terms &amp; Privacy.</p>
-           
             &nbsp; 
-
             </form>
         </div>
         <Footer />
