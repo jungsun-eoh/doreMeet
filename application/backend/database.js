6e32ef3dae2cfeb9ed66c465a0e5001b81fca307
root <root@ip-172-31-9-36.us-west-1.compute.internal>
2020-11-24 04:36:26 +0000

    ec2

--- application/backend/database.js
+++ application/backend/database.js
@@ -1,16 +1,14 @@
-
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
     // changed host for debug. consider changing fields
-    host: "localhost",
+    host: "http://13.52.247.220/",
     user: "root",
-    password: "root",
+    password: "CSC648TEAM02!",
     database: "mydb",
     connectionLimit: 50,
     insecureAuth: true,
     queueLimit: 0
 });
-
 
 module.exports = pool;
