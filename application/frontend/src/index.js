17d695b9759f4426dc5d5b14015b0b85f062d50d
root <root@ip-172-31-9-36.us-west-1.compute.internal>
2020-11-24 04:41:52 +0000

    ec2 2

--- application/frontend/src/index.js
+++ application/frontend/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import axios from 'axios';
 import reportWebVitals from './reportWebVitals';
+
+axios.defaults.baseURL = 'http://ec2-13-52-247-220.us-west-1.compute.amazonaws.com:5000/';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.common = {"X-Requested-With": "XMLHttpRequest","X-CSRFToken": "Origin, X-Requested-With, Content-Type, Accept, Authorization"};
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +17,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
